a396290a1f27ada8d58634bfb99be632f5021ab7
Mrityunjay Samanta <samantamrityunjay98@gmail.com>
2021-05-30 17:01:09 +0000

    deleted deployment

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"email@com\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
